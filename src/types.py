4b42f2d2d33c849916041511d409a10dca4c7d84
jackwang <jack4shihong@gmail.com>
2025-01-09 21:27:53 +0800

    update

---
+++ src/types.py
@@ -0,0 +1,19 @@
+"""
+types.py: 定义项目中使用到的类型。
+
+    @Time    : 2025/01/09
+    @Author  : JackWang
+    @File    : types.py
+    @IDE     : VsCode
+"""
+
+# Standard Library
+from pathlib import Path
+
+# Third-Party Library
+from pydantic import BaseModel
+
+
+class Task(BaseModel):
+    name: str
+    remote_path: Path
