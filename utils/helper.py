20c7ca391239a2c25a703066e2b4d1fd43b0396a
jackwang <jack4shihong@gmail.com>
2025-01-10 15:01:14 +0800

    add get free gpu

--- utils/helper.py
+++ utils/helper.py
@@ -12,6 +12,7 @@ import json
 import base64
 import quopri
 import threading
+import subprocess
 from pathlib import Path
 from typing import Any, Optional
 
@@ -47,9 +48,38 @@ class Product(BaseModel):
     clip_files: Optional[list[Path]] = None
     audio_files: Optional[list[Path]] = None
 
+    # extract_srt
+    zh_srt_files: Optional[list[Path]] = None
+    en_srt_files: Optional[list[Path]] = None
+
 
 def get_thread_id() -> int:
     return threading.get_native_id()
+
+
+def get_free_gpu_indices():
+
+    def run_cmd(cmd):
+        out = (subprocess.check_output(cmd, shell=True)).decode("utf-8")[:-1]
+        return out
+
+    out = run_cmd("nvidia-smi -q -d Memory | grep -A4 GPU")
+    out = (out.split("\n"))[1:]
+    out = [l for l in out if "--" not in l]
+
+    total_gpu_num = int(len(out) / 5)
+    gpu_bus_ids = []
+    for i in range(total_gpu_num):
+        gpu_bus_ids.append([l.strip().split()[1] for l in out[i * 5 : i * 5 + 1]][0])
+
+    out = run_cmd("nvidia-smi --query-compute-apps=gpu_bus_id --format=csv")
+    gpu_bus_ids_in_use = (out.split("\n"))[1:]
+    gpu_ids_in_use = []
+
+    for bus_id in gpu_bus_ids_in_use:
+        gpu_ids_in_use.append(gpu_bus_ids.index(bus_id))
+
+    return [i for i in range(total_gpu_num) if i not in gpu_ids_in_use]
 
 
 def read_json(json_path: Path) -> dict[str, Any]:
